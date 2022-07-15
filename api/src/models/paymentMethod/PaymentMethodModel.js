8fa43abbcfeef2081fb83e9a6a199f17bfa988bd
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-07-16 09:14:57 +1000

    payment method model and schema (GET,POST,PUT,DELETE)

---
+++ api/src/models/paymentMethod/PaymentMethodModel.js
@@ -0,0 +1,24 @@
+import PaymentMethodSchema from "./PaymentMethodSchema";
+
+export const createPaymentMethod = (obj) => {
+  return PaymentMethodSchema(obj).save();
+};
+
+export const getPaymentMethod = () => {
+  return PaymentMethodSchema.find();
+};
+
+// find One
+export const getSinglePaymentMethod = (filter) => {
+  return PaymentMethodSchema.findOne(filter);
+};
+
+// find One and Update
+export const updatePaymentMethod = (filter, update) => {
+  return PaymentMethodSchema.findOneAndUpdate(filter, update, { new: true });
+};
+
+// delete
+export const deletePaymentMethod = (filter) => {
+  return PaymentMethodSchema.findOneAndDelete(filter);
+};
