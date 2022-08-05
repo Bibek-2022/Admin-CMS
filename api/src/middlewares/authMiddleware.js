3cd9f738d195c7e1443ad7f6721bbfc33bda1cce
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-08-05 20:01:03 +1000

    authentication middleware

---
+++ api/src/middlewares/authMiddleware.js
@@ -0,0 +1,49 @@
+import { verifyAccessJWT } from "../helpers/jwtHelper.js";
+import { getOneAdmin } from "../models/adminUser/AdminUserModel.js";
+import { getSession } from "../models/session/SessionModel.js";
+
+export const adminAuth = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    if (authorization) {
+      // is valid token
+      const decoded = verifyAccessJWT(authorization);
+
+      if (decoded === "jwt expired") {
+        return res.status(403).json({
+          status: "erroro",
+          message: "jwt expired",
+        });
+      }
+
+      if (decoded?.email) {
+        // is exist in the session table
+        const existIbDB = await getSession({
+          type: "jwt",
+          tokne: authorization,
+        });
+
+        // get user info and do next
+        if (existIbDB?._id) {
+          const admin = await getOneAdmin({
+            email: decoded.email,
+          });
+
+          if (admin?._id) {
+            req.adminInfo = admin;
+            return next();
+          }
+        }
+      }
+
+      //otherwise return 403 status
+    }
+
+    res.status(401).json({
+      status: "error",
+      message: "Unauthorized",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
