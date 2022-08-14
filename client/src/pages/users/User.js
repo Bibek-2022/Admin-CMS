ac6e5f21064598b70484fc07d2062a198efe942e
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-08-14 12:50:18 +1000

    User FE

--- client/src/pages/users/User.js
+++ client/src/pages/users/User.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { Table } from "react-bootstrap";
 import AdminLayout from "../../layout/AdminLayout";
 
 const User = () => {
-  return <AdminLayout>User</AdminLayout>;
+  return (
+    <AdminLayout>
+      <h4 className="py-5">Customer Management</h4>
+
+      <Table striped hover bordered>
+        <thead>
+          <tr>
+            <td>#</td>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Phone</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+      </Table>
+    </AdminLayout>
+  );
 };
 
 export default User;
