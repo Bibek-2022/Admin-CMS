28b8bfb2e36e3046ddbc63ed1a4be809ee39e23e
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-08-16 22:00:55 +1000

    Product Table

--- client/src/components/product-table/ProductTable.js
+++ client/src/components/product-table/ProductTable.js
@@ -4,66 +4,71 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchProductsAction } from "../../pages/products/productAction";
 import { CustomModal } from "../custom-modal/CustomModal";
 import { Link } from "react-router-dom";
+const API_ROOT_URL = "http://localhost:8000/";
 export const ProductTable = () => {
   const dispatch = useDispatch();
   const [displayProducts, setDisplayProduct] = useState([]);
+
   const { products } = useSelector((state) => state.products);
+
   useEffect(() => {
     !displayProducts.length && dispatch(fetchProductsAction());
     products.length && setDisplayProduct(products);
   }, [products, dispatch, displayProducts]);
 
   return (
-    <Row className="mt-5">
-      <Col>
-        <p>{displayProducts.length}Product Found</p>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>~#</th>
-              <th>Status</th>
-              <th>Name</th>
-              <th>Quanitity</th>
-              <th>Price</th>
-              <th>Sales Price</th>
-              <th>Sales Date</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {displayProducts.length > 0 &&
-              displayProducts.map((item, i) => {
-                return (
+    <div>
+      <Row className="mt-5">
+        <Col>
+          <p>{displayProducts.length} Producs found </p>
+
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th> #</th>
+                <th> Thumbnail</th>
+                <th> Status</th>
+                <th> Name</th>
+                <th>QTY</th>
+                <th>Price</th>
+                <th>Sales Price</th>
+                <th>Sales Date</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {displayProducts.length > 0 &&
+                displayProducts.map((item, i) => (
                   <tr key={item._id}>
                     <td>{i + 1}</td>
                     <td>
-                      <img src={item.thumbnail} alt="" width="150px" />
+                      <img
+                        src={API_ROOT_URL + item.thumbnail}
+                        width="150px"
+                        crossOrigin="anonymous"
+                      />
                     </td>
                     <td>{item.status}</td>
                     <td>{item.name}</td>
-                    <td>{item.price}</td>
-                    <td>{item.salesPrice}</td>
+                    <td>{item.qty}</td>
+                    <td>${item.price}</td>
+                    <td>${item.salesPrice}</td>
                     <td>
-                      {item.saleStartDate.slice(0, 10)}-
-                      {item.saleEndDate.slice(0, 10)}
+                      {item.salesStartDate && item.salesStartDate.slice(0, 10)}{" "}
+                      {" To "}
+                      {item.salesEndDate && item.salesEndDate.slice(0, 10)}
                     </td>
-
                     <td>
                       <Link to={`/product/edit/${item._id}`}>
-                        <Button varient="warning">Edit</Button>
+                        <Button variant="warning">Edit</Button>
                       </Link>
                     </td>
                   </tr>
-                );
-              })}
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-          </tbody>
-        </Table>
-      </Col>
-    </Row>
+                ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </div>
   );
 };
