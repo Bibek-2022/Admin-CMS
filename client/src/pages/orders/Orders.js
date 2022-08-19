d4a897eb233eaba975402f002927e04b474ba57b
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-08-19 23:33:43 +1000

    pagination to be implemented

--- client/src/pages/orders/Orders.js
+++ client/src/pages/orders/Orders.js
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Form, Table } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import AdminLayout from "../../layout/AdminLayout";
+import { getOrderAction } from "./OrderAction";
+
+const ordersPerTable = 10;
 
 const Orders = () => {
+  const dispatch = useDispatch();
+
+  const [displayOrders, setDisplayOrders] = useState([]);
+  const [active, setActive] = useState(1);
+
+  const { orders } = useSelector((state) => state.orders);
+
+  useEffect(() => {
+    !orders.length && dispatch(getOrderAction());
+    setDisplayOrders(orders);
+  }, [dispatch, orders]);
+
+  const handleOnFilter = (e) => {
+    setActive(1);
+    const { value } = e.target;
+    if (value === "all") {
+      setDisplayOrders(orders);
+    } else {
+      setDisplayOrders(orders.filter((item) => item.status === value));
+    }
+  };
+
+  const pages = Math.ceil(displayOrders.length / ordersPerTable);
+  const orderStartAt = (active - 1) * ordersPerTable;
+  const orderEndAt = orderStartAt + ordersPerTable;
+
   return (
     <AdminLayout>
-      <h4>Order Management</h4>
-      <div className="d-flex justify-content-between mb-2">
-        <div className="">50 FOUND</div>
-        <div className="">
-          <Form.Control placeholder="Search By Buyer Name" />
+      <h4 className="py-3">Order management</h4>
+      <div className="d-flex justify-content-between mb-2 mt-4">
+        <div>{displayOrders.length} products found!</div>
+        <div>
+          <Form.Control placeholder="serach by buyer name" />
         </div>
 
-        <div className="">
-          <Form.Select defaultValue="">
-            <option value=""></option>
-            <option value="processing">Processing</option>
-            <option value="shipped">Shipped</option>
-            <option value="cancelled">Cancelled</option>
+        <div>
+          <Form.Select defaultValue="all" onChange={handleOnFilter}>
+            <option value="all"> -- filter --</option>
+            <option value="processing">Processing </option>
+            <option value="shipped">Shipped </option>
+            <option value="cancelled">cancelled </option>
           </Form.Select>
         </div>
       </div>
@@ -30,23 +60,39 @@ const Orders = () => {
             <th>#</th>
             <th>Status</th>
             <th>Buyer Name</th>
-            <th>Order Total</th>
+            <th>Order total</th>
             <th>Payment Status</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Active</td>
-            <td>Bla</td>
-            <td>Bla</td>
-            <td>Bla</td>
-            <td>
-              <Link to={`/orders/${2}`}></Link>
-            </td>
-          </tr>
+          {displayOrders.map(
+            (order, i) =>
+              i >= orderStartAt &&
+              i < orderEndAt && (
+                <tr key={i}>
+                  <td>{i + 1}</td>
+                  <td>{order.status}</td>
+                  <td>
+                    {order.buyer.fName} {order.buyer.lName}
+                  </td>
+                  <td>{order.totalAmount}</td>
+                  <td>{order.paymentInfo.status}</td>
+                  <td>
+                    <Link to={`/orders/${order._id}`}>View Details</Link>
+                  </td>
+                </tr>
+              )
+          )}
         </tbody>
       </Table>
+      <div className="d-flex justify-content-center">
+        <CustomPagination
+          pages={pages}
+          active={active}
+          handleOnPaginationClick={setActive}
+        />
+      </div>
     </AdminLayout>
   );
 };
